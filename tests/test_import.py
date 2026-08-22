40a32176b9f7f63b9cbffc82e8b730e97349a9a8
Sujay Mate <sujay.mate@gmail.com>
2026-08-22 19:26:46 -0400

    add pre commit hooks

--- tests/test_import.py
+++
@@ -1,5 +0,0 @@
-def test_polpy_imports() -> None:
-    try:
-        from polpy import poldata, polresponse, polarizationlike
-    except ImportError:
-        print("Polpy package not imported successfully")
